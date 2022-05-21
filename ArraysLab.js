df329d206595e2cfde9641ee006629ebd287f06c
Asen K <krushkov1914@gmail.comgit$git config --global user.email krushkov1914@gmail.com>
2022-05-21 20:33:47 +0200

    Array Lab done

--- ArraysLab.js
+++ ArraysLab.js
@@ -119,13 +119,92 @@ function pieceOfPie(ples, start, end){
 }
 
 //Process Odd Positions
+function solve(numbers){
+   console.log(numbers
+        .filter((x,i)=> i % 2)
+        .map(x => x * 2)
+        .reverse()
+        .join(' '));
+}
 
 //Biggest Element
+    function solve(input){
+    let newArr = [];
 
+    for(every of input){
+        let current = bigger(every);
+        newArr.push(current);
+    }
+    function bigger(x){
+        let current = x.shift();
+        for(each of x){
+            if(current < each){
+                current = each;
+            }
+        }
+        return current;
+    }
+    newArr = bigger(newArr);
+    console.log(newArr);
+}
 //Diagonal Sums
+    function solve(numbers){
+    matrixLength = numbers.length;
+    let firstDiagonal = 0;
+    let secondDiagonal = 0;
+
+    for(let i =0;i<matrixLength;i++){
+        firstDiagonal += numbers[i][i];
+        secondDiagonal += numbers[i][matrixLength-i-1];
+    }
+    console.log(firstDiagonal + ' ' + secondDiagonal);
+}
 
 //Equal Neighbors
 
+
+function solve(input){
+    let match = [];
+
+    for(let i = 1; i < input.length; i++){
+        let filteredArray = input[i].filter((number, index)=> {
+            return input[i - 1].indexOf(number,index) === index;
+        })
+        match.push(...filteredArray)
+        let filteredArrayOne = input[i - 1].filter((element,index) =>{
+            return element === input[i - 1][index - 1];
+        })
+        match.push(...filteredArrayOne);
+    }
+    let filteredArrayTwo = input[input.length -1].filter((element, index)=>{
+        return element === input[input.length - 1][index - 1];
+    })
+    match.push(...filteredArrayTwo);
+    console.log(match.length);
+}
+
+function solve(input) {
+    let match = 0;
+    for (let x = 0; x < input.length; x++) {
+        for (let y = 0; y < input[x].length - 1; y++) {
+            if(input[x][y] == input[x][y + 1]){
+                match++;
+            }
+        }
+    }
+    for (let y = 0; y < input[0].length; y++) {
+        for (let x = 0; x < input.length - 1; x++) {
+            if(input[x][y] == input[x + 1][y]){
+                match++;
+            }
+        }
+    }
+    console.log(match);
+}
+solve([['2', '3', '4', '7', '0'],
+['4', '0', '5', '3', '4'],
+['2', '3', '5', '4', '2'],
+['9', '8', '7', '5', '4']])
 */
 
 
@@ -147,7 +226,7 @@ function evenPositionElement(input){
     for(let i = 0; i< input.length; i+= 2){
         result.push(input[i]);
     }
-   // console.log(result.join(' '));
+    //console.log(result.join(' '));
 }
  //     first index of splice - 'select the index which u want to work'
  //     second index - 'How manny elements u want to delete 
@@ -186,7 +265,11 @@ const oneToNine = num1.concat(num2,num3);
 let arr = [10,20,30,40,50];
 
 //arr.forEach((value, index, arr) => console.log(value,index,arr));
-
+/* 
+    10 0 (5) [10, 20, 30, 40, 50]
+    20 1 (5) [10, 20, 30, 40, 50]
+    30 2 (5) [10, 20, 30, 40, 50]...
+*/
 
 let nums = arr.map(Number);
 
@@ -194,12 +277,56 @@ let isEven = function(element){
     return element % 2 === 0;
 }
 //console.log(nums.some(isEven));
+// true
 //console.log(nums.some(x => x > 30));
+// true
 // Some check if there is qny true will return true;
 //console.log(nums.every(x => x > 8));
+// true
 // with every u need to have all of them true ;
 
 //console.log(nums.find(x => x>32));
+// 40
 //find will give you the first True!;
 //For index u can use findIndex;
-console.log(nums.findIndex(x => x>32));
+//console.log(nums.findIndex(x => x>32));
+/// 3
+
+pokemons.push('chalizard');
+function filterNames(pokemon, query){
+    return pokemon.filter(function(el){
+        return el.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    })
+}
+// console.log(filterNames(pokemons, 'Cha'));
+// ['Charmander','chalizard']
+// 10, 20, 30, 40, 50
+let newArr = arr.reduce(reducer, 0);
+
+
+function reducer(accumolator,value){
+    return accumolator + value;
+}
+
+//console.log(newArr);
+// 150 
+/// short Version!!!! WHATCH
+
+const result = arr.reduce((a,v) => a + v, 0);
+//console.log(result)
+// 150
+
+// Sum and find the average ; 
+
+let sum = [0,1,2,3].reduce(function(acc,curr){
+    return acc + curr;
+},0);
+// console.log(sum);  / 6
+const average = 
+    arr.reduce((total,number,index,array) =>{
+        total += number;
+        if(index === array.length - 1){
+            return total/array.length;
+        }else{ return total;}
+    })
+    // console.log(average); 30
