ad723fb3ca1364215fb1af739aa92ddb3cc519ff
Asen K <krushkov1914@gmail.comgit$git config --global user.email krushkov1914@gmail.com>
2022-05-28 00:37:09 +0200

    DOM LAB

---
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -0,0 +1,19 @@
+function search() {
+   let towns = Array.from(document.querySelectorAll('li'));
+   let townsString = towns.map(e => e.textContent);
+   let search = document.getElementById('searchText').value;
+   let sum = 0;
+   let result = document.getElementById('result');
+
+   for(let i = 0; i < townsString.length; i++){
+      let curr = townsString[i].split(search);
+      if(curr.length > 1){
+         sum++;
+         towns[i].style.textDecoration = 'underline';
+         towns[i].style.fontWeight = 'bold';
+      }
+   }
+   result.innerHTML = `${sum} matches found`;
+
+}
+
